40bc2e109d5cd02f7cb362209f9de467b5413957
Franco <ufrancis186@gmail.com>
2025-10-31 20:43:37 +0100

    framerr setup

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {motion, useAnimation, useInView} from "framer-motion"
 import heroimg from "../assets/mekus-hero.jpg"
 function Herosection(){
+    const ref = useRef(null);
+    const isInView = useInView(ref);
+    const mainControls = useAnimation();
+    const variants = {
+        hidden:{opacity: 0, x: 75},
+        visible:{opacity: 1, X: 0}
+    }
     return(
         <div className="relative w-full h-[800px]">
             <img 
