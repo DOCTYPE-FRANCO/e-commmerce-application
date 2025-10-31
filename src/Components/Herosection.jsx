76e8db44184e788f9f1873d75c384b92a6b69946
Franco <ufrancis186@gmail.com>
2025-10-31 22:21:18 +0100

    ttom

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -7,8 +7,14 @@ function Herosection(){
     const mainControls = useAnimation();
     const variants = {
         hidden:{opacity: 0, x: 75},
-        visible:{opacity: 1, X: 0}
+        visible:{opacity: 1, x: 0, transition: {duration: 2}}
     }
+
+    useEffect(() =>{
+        if(isInView){
+            mainControls.start("visible");
+        }
+    }, [isInView]);
     return(
         <div className="relative w-full h-[800px]">
             <img 
@@ -17,6 +23,35 @@ function Herosection(){
             />
 
             <div className="absolute inset-0 bg-black/40" />
+
+            <div ref={ref} className="relative z-20 flex flex-col gap-2">
+                <motion.p 
+                    variants={variants}
+                    initial="hidden"
+                    animate={mainControls}
+                    className="text-5xl text-white font-extrabold"
+                >
+                    Elevate Your Style
+                </motion.p>
+
+                <motion.p 
+                    variants={variants}
+                    initial="hidden"
+                    animate={mainControls}
+                    className="text-3xl text-white bold"
+                >
+                    Discover premium clothing designed just for you.
+                </motion.p>
+
+                <motion.p 
+                    variants={variants}
+                    initial="hidden"
+                    animate={mainControls}
+                    className="w-[250px] h-[35px] font-extrabold bg-white hover:scale-110"
+                >
+                    SHOP NOW
+                </motion.p>
+            </div>
         </div>
     );
 }
