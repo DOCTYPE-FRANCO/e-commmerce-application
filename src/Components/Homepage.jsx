7f769307d4b43fd89ddde833912ae4ef661f963a
Franco <ufrancis186@gmail.com>
2025-11-01 21:54:14 +0100

    xx

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import Herosection from "./Herosection";
-import PlainTee from "../assets/"
+import PlainTee from "../assets/plain.jpg"
+import Jean from "../assets/baggyjean.jpg"
+import Joggers from "../assets/joggers.jpg"
+import Jacket from "../assets/jacket.jpeg"
+import Cap from "../assets/hats.jpeg"
+import Glasses from "../assets/shades.jpeg"
+import Shoes from "../assets/shoes.jpeg"
+import Slides from "../assets/slides.jpeg"
+import Boxers from "../assets/boxers.jpeg"
 function Homepage(){
     return(
         <div className="z-30">
