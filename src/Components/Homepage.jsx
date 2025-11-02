bee6d0a6026e118d1217cdc4ecd3c1656a718c97
Franco <ufrancis186@gmail.com>
2025-11-02 21:42:59 +0100

    array of product fr display

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -9,10 +9,54 @@ import Glasses from "../assets/shades.jpeg"
 import Shoes from "../assets/shoes.jpeg"
 import Slides from "../assets/slides.jpeg"
 import Boxers from "../assets/boxers.jpeg"
+
 function Homepage(){
+
+    const products = [
+        {
+            id: 1,
+            name : "Plain T-shirt",
+            image: PlainTee,
+        },
+
+        {
+            id: 2,
+            name : "Baggy Jeans",
+            image: Jean,
+        },
+
+        {
+            id: 3,
+            name : "Joggers",
+            image: Joggers,
+        },
+
+        {
+            id: 4,
+            name : "Jacket",
+            image: Jacket,
+        },
+
+        {
+            id: 5,
+            name : "Cap",
+            image: Cap,
+        },
+
+        {
+            id: 6,
+            name : "Glasses",
+            image: Glasses,
+        },
+    ]
+
     return(
         <div className="z-30">
             <Herosection />
+
+           <div>
+
+           </div>
         </div>
     );
 }
