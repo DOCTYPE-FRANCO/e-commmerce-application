f34ef07ca1b317c49be74d27c862a2f63bc7ed13
Franco <ufrancis186@gmail.com>
2025-11-03 02:25:04 +0100

    z

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import { MenuIcon, XIcon } from "lucide-react";
 function Navbar(){
     const [tab, setTab] = useState(false);
     return(
-        <div className="md:pr-20 pr-5">
+        <div className="md:pr-20 pr-2">
             <ul className="hidden md:flex flex-row gap-10 justify-center items-center">
                 <p className="font-bold hover:text-gray-500 hover:cursor-pointer">Home</p>
                 <p className="font-bold hover:text-gray-500 hover:cursor-pointer">Shop</p>
@@ -16,7 +16,7 @@ function Navbar(){
             </div>
 
             {tab && (
-                <div onClick={() => alert("No Products Yet!")} className="flex justify-center items-center  backdrop-blur-sm top-20 right-10 fixed bg-gray-700/80 w-[150px] h-[170px] rounded-md">
+                <div onClick={() => alert("No Products Yet!")} className="flex justify-center items-center  backdrop-blur-sm top-20 right-10 fixed bg-gray-500/80 w-[150px] h-[170px] rounded-md">
                     <ul className="flex flex-col text-white font-bold gap-5 justify-center items-center">
                         <p className="font-bold hover:text-gray-500 hover:cursor-pointer">Home</p>
                         <p className="font-bold hover:text-gray-500 hover:cursor-pointer">Shop</p>
