67655534d4a54d1afb0244fc9e49ee6892cc79cd
Franco <ufrancis186@gmail.com>
2025-11-03 02:22:52 +0100

    x

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,8 @@ import Navbar from "./Navbar";
 import Logo from "../assets/logo.png"
 function Header(){
     return(
-        <div className="mx-auto fixed top-4 z-50 w-[80vw] h-[60px] bg-gray-200/80 flex justify-between items-center justify-self-center rounded-2xl ">
-            <img src={Logo} className="w-[60px] ml-10" />
+        <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 w-[80vw] h-[60px] bg-gray-200/80 flex items-center justify-between rounded-2xl px-6">
+            <img src={Logo} className="w-[60px]" />
             <Navbar />
         </div>
     );
