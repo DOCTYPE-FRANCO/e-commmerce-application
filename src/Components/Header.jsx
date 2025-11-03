da8e95cc354739e3d74f4ab44a4963a8f3b81f2c
Franco <ufrancis186@gmail.com>
2025-11-03 02:26:06 +0100

    all single lettters simply mean i was running test caus i already deployed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 import Logo from "../assets/logo.png"
 function Header(){
     return(
-        <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 w-[80vw] h-[60px] bg-gray-200/80 flex items-center justify-between rounded-2xl px-6">
+        <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 w-[85vw] h-[60px] bg-gray-200/80 flex items-center justify-between rounded-2xl px-6">
             <img src={Logo} className="w-[60px]" />
             <Navbar />
         </div>
