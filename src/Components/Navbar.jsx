7e484bfc0837ee11ad91aba0dc13437936fe54f3
Franco <ufrancis186@gmail.com>
2025-11-03 01:17:00 +0100

    lucide reactt installed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { MenuIcon } from "lucide-react";
 
 function Navbar(){
     return(
         <div className="pr-20">
-            <ul className="flex flex-row gap-10 justify-center items-center">
+            <ul className="hidden md:flex flex-row gap-10 justify-center items-center">
                 <p className="font-bold hover:text-gray-500 hover:cursor-pointer">Home</p>
                 <p className="font-bold hover:text-gray-500 hover:cursor-pointer">Shop</p>
                 <p className="font-bold hover:text-gray-500 hover:cursor-pointer">Cart</p>
             </ul>
+
+            <div>
+                <MenuIcon />
+            </div>
         </div>
     );
 }
